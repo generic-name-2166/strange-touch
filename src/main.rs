652fe5a4ba2313aaa3e45af86efbc1c91af5fd64
generic-name-2166 <kirbesson277@mail.ru>
2025-05-08 17:17:48 +0300

    feat!: specify timestamp instead

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::{fs::File, os::windows::io::AsRawHandle as _, path::PathBuf};
+use std::{
+    fs::{File, OpenOptions},
+    os::windows::io::AsRawHandle as _,
+    path::PathBuf,
+};
 
-use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use clap::Parser;
-use color_eyre::eyre;
+use color_eyre::eyre::{self, OptionExt as _};
 use windows::Win32::{
     Foundation::{FILETIME, HANDLE},
     Storage::FileSystem::SetFileTime,
 };
 
 #[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct CliArgs {
     path: PathBuf,
-    datetime: String,
+    #[arg(short, long)]
+    timestamp: Option<i64>,
+    #[arg(short, long)]
+    datetime: Option<String>,
 }
 
 /// Seconds between 1601-01-01 and 1970-01-01
@@ -26,11 +34,12 @@ fn set_filetime(handle: HANDLE, datetime: DateTime<Utc>) {
 }
 
 fn parse_date(date_string: &str) -> eyre::Result<DateTime<Utc>> {
-    let naive_date: NaiveDate = match NaiveDate::parse_from_str(date_string, "%Y-%m-%d") {
-        Err(error) => Err(eyre::eyre!("failure to parse {} - {}", date_string, error)),
-        Ok(ok) => Ok(ok),
-    }?;
-    let datetime: DateTime<Utc> = NaiveDateTime::new(naive_date, NaiveTime::MIN).and_utc();
+    let datetime: DateTime<Utc> =
+        match NaiveDateTime::parse_from_str(date_string, "%Y-%m-%d %H:%M:%S") {
+            Err(error) => Err(eyre::eyre!("failure to parse {} - {}", date_string, error)),
+            Ok(ok) => Ok(ok),
+        }?
+        .and_utc();
     Ok(datetime)
 }
 
@@ -50,9 +59,15 @@ fn datetime_to_filetime(datetime: DateTime<Utc>) -> FILETIME {
 fn main() -> eyre::Result<()> {
     let args: CliArgs = CliArgs::parse();
 
-    let datetime: DateTime<Utc> = parse_date(&args.datetime)?;
+    let datetime = match (args.timestamp, args.datetime) {
+        (Some(secs), _) => {
+            DateTime::from_timestamp(secs, 0).ok_or_eyre("provided timestamp is invalid")
+        }
+        (None, Some(date_string)) => parse_date(&date_string),
+        _ => Err(eyre::eyre!("provide either --timestamp or --datetime")),
+    }?;
 
-    let file: File = File::open(args.path)?;
+    let file: File = OpenOptions::new().write(true).open(args.path)?;
     let handle: HANDLE = HANDLE(file.as_raw_handle());
 
     set_filetime(handle, datetime);
